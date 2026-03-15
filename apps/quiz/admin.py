cf189b913a504d4427f830e1db92bb6e1baa4f0c
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-03-15 11:57:52 +0600

    create auiz/admin.py

--- apps/quiz/admin.py
+++ apps/quiz/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Quiz, Question, Result
 
-# Register your models here.
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    inlines = [QuestionInline]
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("id", "quiz", "question_text", "correct_option")
+    list_filter = ("quiz",)
+    search_fields = ("question_text",)
+
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "quiz", "score")
+    list_filter = ("quiz", "user")
