68c7fd3f7d0cff46737842b6275333030b206a98
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-03-15 11:52:07 +0600

    create auiz/models.py

--- apps/quiz/models.py
+++ apps/quiz/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name="questions")
+    question_text = models.TextField()
+    option_a = models.CharField(max_length=100)
+    option_b = models.CharField(max_length=100)
+    option_c = models.CharField(max_length=100)
+    option_d = models.CharField(max_length=100)
+    correct_option = models.CharField(max_length=1)
+
+class Result(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    score = models.IntegerField()
