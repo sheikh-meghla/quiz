9febb90635206b19d351a3bc03cbed673aade3ec
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-03-15 20:46:03 +0600

    add unfold

--- apps/quiz/admin.py
+++ apps/quiz/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Quiz, Question, Result
+from unfold.admin import ModelAdmin
 
 
 class QuestionInline(admin.TabularInline):
@@ -8,19 +9,19 @@ class QuestionInline(admin.TabularInline):
 
 
 @admin.register(Quiz)
-class QuizAdmin(admin.ModelAdmin):
+class QuizAdmin(ModelAdmin):
     list_display = ("id", "title")
     inlines = [QuestionInline]
 
 
 @admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
+class QuestionAdmin(ModelAdmin):
     list_display = ("id", "quiz", "question_text", "correct_option")
     list_filter = ("quiz",)
     search_fields = ("question_text",)
 
 
 @admin.register(Result)
-class ResultAdmin(admin.ModelAdmin):
+class ResultAdmin(ModelAdmin):
     list_display = ("id", "user", "quiz", "score")
     list_filter = ("quiz", "user")
